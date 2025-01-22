5e6aa0e40d3374059b4fef176997e0d26fa5694b
Kirill Krasnikov <k.krasnikov@napopravku.ru>
2025-01-22 18:24:10 +0300

    issue-2 fix from and to params in AppointmentListDTO

--- src/Factories/Appointment/Request/AppointmentListFactory.php
+++ src/Factories/Appointment/Request/AppointmentListFactory.php
@@ -2,30 +2,11 @@
 
 namespace Napopravku\RtaApiClient\Factories\Appointment\Request;
 
-use Carbon\Carbon;
 use Napopravku\RtaApiClient\Factories\Common\AbstractListFactory;
 use Napopravku\RtaApiClient\DTOs\Appointment\Request\AppointmentListDTO;
 
 class AppointmentListFactory extends AbstractListFactory
 {
-    public static function setFrom(?string $value): ?Carbon
-    {
-        if (!$value) {
-            return null;
-        }
-
-        return Carbon::parse($value);
-    }
-
-    public static function setTo(?string $value): ?Carbon
-    {
-        if (!$value) {
-            return null;
-        }
-
-        return Carbon::parse($value);
-    }
-
     public static function getDTO(): AppointmentListDTO
     {
         return new AppointmentListDTO();
